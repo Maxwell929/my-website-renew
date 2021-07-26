5a5bd2033cc129ab6198622bfa5aa0a9ac93dea4
Maxwell929 <maximilian9219@gmail.com>
2021-07-26 22:16:18 +0200

    created projects section

--- src/components/projects/card.js
+++ src/components/projects/card.js
@@ -4,7 +4,9 @@ import ReactCardFlip from 'react-card-flip';
 import "./cards/cards.css";
 import "./projects.css";
 import Fade from 'react-reveal/Fade';
-import data from "./../../data.json"
+import data from "./../../data.json";
+import { FaGithub } from 'react-icons/fa';
+import { BiWorld } from "react-icons/bi";
 
 
 
@@ -12,9 +14,11 @@ import data from "./../../data.json"
 const Card = () => {
 
 
-    const [img, title, year] = data.projects
+
 
     let time = 0
+
+
 
     return (
         <section id="projects" className="projects">
@@ -23,20 +27,54 @@ const Card = () => {
 
                 {data.projects.map((d) => {
                     time = time + 300;
+                    let webpage;
+                    let github;
+                    if (d.website == "") {
+                        webpage = <a></a>;
+                    }
+
+                    else {
+                        webpage = <a href={d.website} target={"_blank"}>
+                            <BiWorld className="logos" />
+                        </a>
+                    };
+
+                    if (d.github == "") {
+                        github = <a></a>;
+                    }
+
+                    else {
+                        github =
+                            <a href={d.github} target={"_blank"}><FaGithub className="logos" /></a>
+
+                    };
+
                     return (
                         <Fade bottom duration={1500} delay={time} distance="400px" >
                             <div className="">
                                 <div class="flip-card" >
                                     <div class="flip-card-inner">
                                         <div class="flip-card-front">
-                                            <p>{d.title}</p>
-                                            <p>{d.year}</p>
+                                            <h6>{d.title}</h6>
+
+                                            <img className="img-card" src={d.img} />
 
                                         </div>
                                         <div class="flip-card-back">
-                                            <h3>Rock paper scisors</h3>
-                                            <p>2012</p>
-                                            <p>We love that guy</p>
+                                            <div className="box-back">
+                                                <div className="card-container">
+                                                    <h6>{d.title}</h6>
+                                                    <div className="box-logos">
+                                                        {github}
+                                                        {webpage}
+                                                    </div>
+
+                                                </div>
+                                                <div className="card-container">
+                                                    <p className="container-desc">{d.desc}</p>
+                                                    <img className="img-card-back" src={d.img} />
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
