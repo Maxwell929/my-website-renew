7ea74706892ed997edd3e7bf56cf055e5b7d41a3
Maxwell929 <maximilian9219@gmail.com>
2021-07-27 11:19:12 +0200

    modified header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -3,20 +3,15 @@ import herader1 from "./header.png";
 import herader2 from "./header2.png";
 import data from "./../../data.json";
 import "./header.css";
-
+import { Link } from "react-scroll";
 import Fade from 'react-reveal/Fade';
 import TextLoop from "react-text-loop";
-
-
 
 
 const Header = () => {
 
   return (
-
     <section id="header" className="header">
-
-
       <Fade top duration={1500} delay={900} distance="400px">
         <div className="box-text">
           <p>{data.p1} </p>
@@ -49,9 +44,8 @@ const Header = () => {
         </Fade>
       </div>
 
-
       <Fade duration={0} delay={2200}  >
-        <>
+        <Link to="about" smooth duration={1000} smooth={true}><>
           <p>{data.scroll_down}</p>
           <svg class="arrows">
             <path class="a1" d="M0 0 L30 32 L60 0"></path>
@@ -59,7 +53,9 @@ const Header = () => {
             <path class="a3" d="M0 40 L30 72 L60 40"></path>
           </svg>
         </>
+        </Link>
       </Fade>
+
     </section>
   );
 };
