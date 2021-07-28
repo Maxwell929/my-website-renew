47396b0975165c605c4084f786451cb83de76ee0
Maxwell929 <maximilian9219@gmail.com>
2021-07-28 11:00:36 +0200

    added footer

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,25 @@
+import React from "react";
+import data from "./../../data.json";
+import { FaGithub } from 'react-icons/fa';
+import "./footer.css";
+import ReactTooltip from "react-tooltip";
+
+
+
+const Footer = () => {
+  return (
+    <div className="footer">
+       <a href={data.nav_github} target={"_blank"}><FaGithub className="logos-nav" data-tip
+                    data-for="Github" /></a>
+
+       
+        <ReactTooltip id="Github" place="top" effect="solid" >
+            Github
+        </ReactTooltip>
+
+      <p className="footer-name">2021 Maximilian Stelzl</p>
+    </div>
+  );
+};
+
+export default Footer;
