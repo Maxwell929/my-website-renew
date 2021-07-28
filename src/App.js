37a269d15b4000491e0f95db5b0562377f742cd0
Maxwell929 <maximilian9219@gmail.com>
2021-07-28 11:00:09 +0200

    solved with issues

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from "../src/components/Header/header";
 import Navbar from './components/Navbar/nav';
 import About from './components/about/about';
 import Cards from './components/projects/card';
+import Footer from "./components/Footer/footer";
 
 function App() {
   return (
     <div className="App">
-
-      <Navbar />
-      <Header></Header>
-      <About></About>
-      <Cards></Cards>
+      <Navbar/>
+      <Header/>
+      <About/>
+      <Cards/>
+      <Footer/>
     </div>
   );
 }
