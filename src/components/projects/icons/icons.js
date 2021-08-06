e31c7d93d7e7108800b1bcac38a533d72081c3b2
Maxwell929 <maximilian9219@gmail.com>
2021-08-06 15:42:01 +0200

    new

--- src/components/projects/icons/icons.js
+++ src/components/projects/icons/icons.js
@@ -1,89 +1,83 @@
 import { React, useState } from "react";
-import Fade from "react-reveal/Fade";
 import { FaGithub } from "react-icons/fa";
 import { BiWorld } from "react-icons/bi";
 import ReactTooltip from "react-tooltip";
 
 const Cards = ({ data }) => {
+  let webpage;
+  let github;
 
+  let tooltipWeb = (
+    <ReactTooltip id="Website" place="top" effect="solid">
+      Website
+    </ReactTooltip>
+  );
 
-    let webpage;
-    let github;
+  let tooltipGit = (
+    <ReactTooltip id="Github" place="top" effect="solid">
+      Github
+    </ReactTooltip>
+  );
 
-    let tooltipWeb = (
-        <ReactTooltip id="Website" place="top" effect="solid">
-            Website
-        </ReactTooltip>
+  if (data.website === "") {
+    webpage = "";
+  } else {
+    webpage = (
+      <>
+        <a
+          href={data.website}
+          target={"_blank"}
+          rel="noopener"
+          data-tip
+          data-for="Website"
+        >
+          <BiWorld className="logos" />
+          {tooltipWeb}
+        </a>
+      </>
     );
+  }
 
-    let tooltipGit = (
-        <ReactTooltip id="Github" place="top" effect="solid" >
-            Github
-        </ReactTooltip>
+  if (data.github === "") {
+    github = "";
+  } else {
+    github = (
+      <>
+        <a
+          href={data.github}
+          target={"_blank"}
+          rel="noopener"
+          data-tip
+          data-for="Github"
+        >
+          <FaGithub className="logos" />
+        </a>
+        {tooltipGit}
+      </>
     );
+  }
 
-    if (data.website === "") {
-        webpage = "";
-    } else {
-        webpage = (
-            <>
-                <a
-                    href={data.website}
-                    target={"_blank"}
-                    rel="noopener"
-                    data-tip
-                    data-for="Website"
-                >
-                    <BiWorld className="logos" />{tooltipWeb}
-                </a>
-
-            </>
-        );
-    }
-
-    if (data.github === "") {
-        github = "";
-    } else {
-        github = (
-            <>
-                <a
-                    href={data.github}
-                    target={"_blank"}
-                    rel="noopener"
-                    data-tip
-                    data-for="Github"
-                >
-                    <FaGithub className="logos" />
-                </a>
-                {tooltipGit}
-            </>
-        );
-    }
-
-    return (
-        <>
-
-            <div className="">
-                <div class="flip-card" >
-                    <div className="box-back">
-                        <div className="card-container">
-                            <p className="header-card">{data.title}</p>
-                            <div className="box-logos">
-                                {github}
-                                {webpage}
-                            </div>
-                        </div>
-                        <div className="card-container">
-                            <p className="container-desc">{data.desc}</p>
-                            <img className="img-card-back" alt={data.img} src={data.img} />
-                        </div>
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="">
+        <div class="flip-card">
+          <div className="box-back">
+            <div className="card-container">
+              <p className="header-card">{data.title}</p>
+              <div className="box-logos">
+                {github}
+                {webpage}
+              </div>
             </div>
-
-        </>
-    );
+            <div className="card-container">
+              <p className="container-desc">{data.desc}</p>
+              <img className="img-card-back" alt={data.img} src={data.img} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
-
 
 export default Cards;
