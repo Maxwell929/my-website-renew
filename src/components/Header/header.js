d132304d6a2f2329a6cf5938db56d30fc6f21fe0
Maximilian <maximilian9219@gmail.com>
2022-01-12 15:31:22 +0100

    media querry header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,50 +1,49 @@
 import React from "react";
 import herader1 from "./header.png";
-import herader2 from "./header2.png";
 import data from "./../../data.json";
 import "./header.css";
-import { Link } from "react-scroll";
+import {Link} from "react-scroll";
 import Fade from 'react-reveal/Fade';
 
 import Loop from "./Loop/loop"
 
 const Header = () => {
 
-  return (
-    <section id="header" className="header">
-      <Fade top duration={1500} delay={900} distance="400px">
-        <div className="box-text">
-          <p>{data.p1} </p>
-          <p>{data.p2}</p>
-        </div>
-      </Fade>
+    return (
+        <section id="header" className="header">
+            <Fade top duration={1500} delay={900} distance="400px">
+                <div className="box-text">
+                    <p>{data.p1} </p>
+                    <p>{data.p2}</p>
+                </div>
+            </Fade>
 
-      <Fade top duration={1500} delay={1300} distance="400px">
-        <Loop />
-      </Fade>
+            <Fade top duration={1500} delay={1300} distance="400px">
+                <Loop/>
+            </Fade>
 
-      <div className="box-image">
-        <Fade left duration={1500} delay={1800} distance="400px">
-          <div >
-            <img className="image-header" src={herader1} alt="coder" />
-          </div>
-        </Fade>
-      </div>
+            <div className="box-image">
+                <Fade left duration={1500} delay={1800} distance="400px">
+                    <div>
+                        <img className="image-header" src={herader1} alt="coder"/>
+                    </div>
+                </Fade>
+            </div>
 
-      <Fade duration={0} delay={2200}  >
-        <Link to="about" smooth duration={1000} smooth={true}><>
+            <Fade duration={0} delay={2200}>
+                <Link to="about" smooth duration={1000} smooth={true}>
+                    <>
+                        <svg className="arrows">
+                            <path className="a1" d="M0 0 L30 32 L60 0"/>
+                            <path className="a2" d="M0 20 L30 52 L60 20"/>
+                            <path className="a3" d="M0 40 L30 72 L60 40"/>
+                        </svg>
+                    </>
+                </Link>
+            </Fade>
 
-          <svg className="arrows">
-            <path className="a1" d="M0 0 L30 32 L60 0"/>
-            <path className="a2" d="M0 20 L30 52 L60 20"/>
-            <path className="a3" d="M0 40 L30 72 L60 40"/>
-          </svg>
-        </>
-        </Link>
-      </Fade>
-
-    </section>
-  );
+        </section>
+    );
 };
 
 export default Header;
