02fed02272420fdecd429bfa09aa8468cd8f00a4
Maximilian <maximilian9219@gmail.com>
2022-01-12 14:54:32 +0100

    updated packages and header

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -20,7 +20,6 @@ const Header = () => {
       </Fade>
 
       <Fade top duration={1500} delay={1300} distance="400px">
-
         <Loop />
       </Fade>
 
@@ -30,22 +29,15 @@ const Header = () => {
             <img className="image-header" src={herader1} alt="coder" />
           </div>
         </Fade>
-
-        <Fade right duration={1500} delay={2200} distance="400px">
-          <div >
-            <img className="image-header" src={herader2} alt="computer" />
-          </div>
-        </Fade>
       </div>
 
       <Fade duration={0} delay={2200}  >
         <Link to="about" smooth duration={1000} smooth={true}><>
 
-          <svg class="arrows">
-            <path class="a1" d="M0 0 L30 32 L60 0"></path>
-            <path class="a2" d="M0 20 L30 52 L60 20"></path>
-            <path class="a3" d="M0 40 L30 72 L60 40"></path>
-
+          <svg className="arrows">
+            <path className="a1" d="M0 0 L30 32 L60 0"/>
+            <path className="a2" d="M0 20 L30 52 L60 20"/>
+            <path className="a3" d="M0 40 L30 72 L60 40"/>
           </svg>
         </>
         </Link>
