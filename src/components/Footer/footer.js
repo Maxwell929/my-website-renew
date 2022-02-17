1e7ff674fe88d6ca6aa50cee421d82cf77354359
Maximilian <maximilian9219@gmail.com>
2022-02-17 10:26:50 +0100

    modified footer

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,30 +1,26 @@
-import React from "react";
-import data from "./../../data.json";
+import React from 'react';
+import data from './../../data.json';
 import {FaGithub, FaLinkedin} from 'react-icons/fa';
-import "./footer.scss";
-import ReactTooltip from "react-tooltip";
+import './footer.scss';
+import ReactTooltip from 'react-tooltip';
 
 
 const Footer = () => {
     return (
-        <div className="footer">
-            <a href={data.nav_github} target={"_blank"} rel="noopener"><FaGithub className="logos-nav" data-tip
-                                                                                 data-for="Github"/></a>
-            <a href={data.nav_linkedIn} target={"_blank"} rel="noopener"><FaLinkedin className="logos-nav" data-tip
-                                                                                 data-for="LinkedIn"/></a>
+        <footer className='footer'>
+            <a href={data.nav_github} target={'_blank'} rel='noopener'><FaGithub className='logos__footer' data-tip
+                                                                                 data-for='Github'/></a>
+            <a href={data.nav_linkedIn} target={'_blank'} rel='noopener'><FaLinkedin className='logos__footer' data-tip
+                                                                                     data-for='LinkedIn'/></a>
 
-
-            <ReactTooltip id="Github" place="top" effect="solid">
+            <ReactTooltip id='Github' place='top' effect='solid'>
                 Github
             </ReactTooltip>
-            <ReactTooltip id="LinkedIn" place="top" effect="solid">
+            <ReactTooltip id='LinkedIn' place='top' effect='solid'>
                 LinkedIn
             </ReactTooltip>
-
-            <p className="footer-name">2021 Maximilian Stelzl</p>
-
-
-        </div>
+            <p className='footer-name'>{new Date().getFullYear()} Maximilian Stelzl</p>
+        </footer>
     );
 };
 
