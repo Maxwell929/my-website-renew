e2c1a049abcff0e865fa13b299083bf74489b120
Maximilian <maximilian9219@gmail.com>
2022-02-17 10:27:02 +0100

    modified hero

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,48 +1,41 @@
-import React from "react";
-import herader1 from "./header.png";
-import data from "./../../data.json";
-import "./header.scss";
-import {Link} from "react-scroll";
+import React from 'react';
+import hero1 from './header.png';
+import data from './../../data.json';
+import './header.scss';
+import './Scroll Down/scroll_down.scss'
+import {Link} from 'react-scroll';
 import Fade from 'react-reveal/Fade';
 
-import Loop from "./Loop/loop"
+import Loop from './Loop/loop'
 
 const Header = () => {
 
     return (
-        <section id="header" className="header">
-            <Fade top duration={1500} delay={900} distance="400px">
-                <div className="box-text">
-                    <p>{data.p1} </p>
-                    <p>{data.p2}</p>
+        <Fade d>
+            <section id='header' className='header'>
+                <div className='container container__header'>
+                    <Fade top duration={1500} delay={900} distance='400px'>
+                        <p>{data.p1} </p>
+                        <p>{data.p2}</p>
+                        <Loop/>
+                        <img className='image-header' src={hero1} alt='coder'/>
+                    </Fade>
                 </div>
-            </Fade>
+                <Link to='about' offset={-60} duration={1000} smooth={true}>
+                    <div className='mouse_scroll'>
+                        <div className='mouse'>
+                            <div className='wheel'/>
+                        </div>
 
-            <Fade top duration={1500} delay={1300} distance="400px">
-                <Loop/>
-            </Fade>
-
-            <div className="box-image">
-                <Fade left duration={1500} delay={1800} distance="400px">
-                    <div>
-                        <img className="image-header" src={herader1} alt="coder"/>
+                        <div>
+                            <span className='m_scroll_arrows one'/>
+                            <span className='m_scroll_arrows two'/>
+                            <span className='m_scroll_arrows three'/>
+                        </div>
                     </div>
-                </Fade>
-            </div>
-
-            <Fade duration={0} delay={2200}>
-                <Link to="about" smooth duration={1000} smooth={true}>
-                    <>
-                        <svg className="arrows">
-                            <path className="a1" d="M0 0 L30 32 L60 0"></path>
-                            <path className="a2" d="M0 20 L30 52 L60 20"></path>
-                            <path className="a3" d="M0 40 L30 72 L60 40"></path>
-                        </svg>
-                    </>
                 </Link>
-            </Fade>
-
-        </section>
+            </section>
+        </Fade>
     );
 };
 
