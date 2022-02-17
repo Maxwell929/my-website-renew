d944b252e358a42c2a9891b3cc35ef5543cbaa36
Maximilian <maximilian9219@gmail.com>
2022-02-17 10:26:40 +0100

    modified nav

--- src/components/Navbar/nav.js
+++ src/components/Navbar/nav.js
@@ -11,21 +11,21 @@ const Navbar = () => {
     return (
         <section className='nav'>
             <div className='container navbar__container'>
-                <Fade top duration={1000} delay={0} distance='50px'>
-                    <Link to='header' smooth duration={1000} smooth={true}>
-                        <img src={logo} alt={logo} className='logo'></img>
+                <Fade top duration={1000} delay={0}  distance='200' >
+                    <Link to='header' offset={-80}  duration={1000} smooth={true}>
+                        <img src={logo} alt={logo} className='logo'/>
                     </Link>
                 </Fade>
 
                 <div className={'nav-links'}>
                     <Fade top duration={1000} delay={0} distance='50px'>
-                        <Link to='about' smooth duration={1000} smooth={true}>
-                            <button class='btn-4'><span>{data.nav1}</span></button>
+                        <Link to='about' offset={-60}  duration={1000} smooth={true}>
+                            <button className='btn-4'><span>{data.nav1}</span></button>
                         </Link>
                     </Fade>
                     <Fade top duration={1000} delay={300} distance='50px'>
-                        <Link to='projects' smooth duration={1000} smooth={true}>
-                            <button class='btn-4'><span>{data.nav2}</span></button>
+                        <Link to='projects' offset={0}  duration={1000} smooth={true}>
+                            <button className='btn-4'><span>{data.nav2}</span></button>
                         </Link>
                     </Fade>
                     <div className='container-logos'>
