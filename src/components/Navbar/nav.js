2202f4a7ac1354f38ea94944af973c580bb2fd95
Maximilian <maximilian9219@gmail.com>
2022-03-30 15:50:42 +0200

    style changes

--- src/components/Navbar/nav.js
+++ src/components/Navbar/nav.js
@@ -16,18 +16,18 @@ const Navbar = () => {
                             <img src={logo} alt={logo} className='logo'/>
                     </Link>
 
-                <div className={'nav-links'}>
+                <ul className={'nav-links'}>
                         <Link className="nav__link" to='about' offset={-60} duration={1000} smooth={true} spy={true} activeClass="active">
                             {data.nav1}
                         </Link>
-                        <Link className="nav__link" to='projects' offset={-10} duration={1000} smooth={true} spy={true} activeClass="active">
+                        <Link className="nav__link" to='projects' offset={-0} duration={1000} smooth={true} spy={true} activeClass="active">
                             {data.nav2}
                         </Link>
                     <div className='container-logos'>
                         <a href={data.nav_github} rel='noopener' target={'_blank'}><FaGithub className='logos-nav'/></a>
                         <a href={data.nav_linkedIn} rel='noopener' target={'_blank'}><FaLinkedin className='logos-nav'/></a>
                     </div>
-                </div>
+                </ul>
             </div>
         </nav>
         </Fade>
